b0d38cbd6e1916e82bd788fbf5039ded38e97e77
USER <USER@DESKTOP-CRJK2GJ>
2021-05-10 23:22:16 +0900

    디스크컨트롤러, 기능개발 풀이

--- src/problem/기능개발/Solution.java
+++ src/problem/기능개발/Solution.java
@@ -1,12 +1,54 @@
 package problem.기능개발;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 class Solution {
 	public static void main(String[] args) {
-		
+		int[] progresses = new int[] { 1,0 };
+		int[] speeds = new int[] { 1,1};
+		int[] answer = new Solution().solution(progresses, speeds);
+		for (int i = 0; i < answer.length; i++) {
+			System.out.println(answer[i]);
+		}
 	}
-	
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        return answer;
-    }
+
+	public int[] solution(int[] progresses, int[] speeds) {
+		ArrayList<Integer> releaseList = new ArrayList<Integer>();
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int i = progresses.length - 1; i >= 0; i--) {
+			stack.push(progresses[i]);
+		}
+		int[] processed = new int[speeds.length];
+		for (int i = 0; i < processed.length; i++) {
+			processed[i] = speeds[i];
+		}
+
+		int processIdx = 0;
+		while (!stack.isEmpty()) {
+			int releaseAvailable = 0;
+			while (!stack.isEmpty()) {
+				int progress = stack.pop();
+				if (progress + processed[processIdx] >= 100) {
+					processIdx++;
+					releaseAvailable++;
+				} else {
+					stack.push(progress);
+					break;
+				}
+			}
+			if (releaseAvailable > 0) {
+				releaseList.add(releaseAvailable);
+			}
+			for (int i = 0; i < processed.length; i++) {
+				processed[i] += speeds[i];
+			}
+		}
+
+		int[] answer = new int[releaseList.size()];
+		for (int i = 0; i < releaseList.size(); i++) {
+			answer[i] = releaseList.get(i);
+		}
+		return answer;
+	}
 }
